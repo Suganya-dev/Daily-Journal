6fbbc1313f5ce2fcded7038f83da33d3d86eba8b
Suganya <suganprakash0621@gmail.com>
2020-10-12 11:39:55 -0500

    Initial commit

---
+++ scripts/JournalDataProvider.js
@@ -0,0 +1,32 @@
+const journal = [
+    {
+        id: 1,
+        date: "07/24/2025",
+        concept: "HTML & CSS",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ok"
+    },
+    {
+        id: 2,
+        date: "08/13/2025",
+        concept: "Complex flex box",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ok"
+    },
+    {
+        id: 3,
+        date: "09/24/2025",
+        concept: "Front end program",
+        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
+        mood: "Ok"
+    }
+]
+
+export const useJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) => {
+    console.log(currentEntry,nextEntry) 
+    Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+        })
+    return sortedByDate
+}
