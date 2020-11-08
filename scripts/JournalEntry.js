b42883ce97d861db54b77b8d11e3fed04f8b2323
Suganya <suganprakash0621@gmail.com>
2020-11-07 22:22:17 -0600

    Got output

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,10 +1,11 @@
  export const JournalEntryComponent = (entriesObj) => {
+     
     return `
         <div class="journal" id="${entriesObj.id}>
-      <p class="journal__date> date: ${new Date(entriesObj.date).toLocaleDateString('en-US')}</p>
-       <p class="journal__concept>concept: ${entriesObj.concept}</p>
-       <p class="journal__entry> entry: ${entriesObj.entry} </p>
-        <p class="journal__mood> mood: ${entriesObj.mood} </p>
+      <p class="journal__date> Date: ${new Date(entriesObj.date).toLocaleDateString('en-US')}</p>
+       <p class="journal__concept>Concept: ${entriesObj.concept}</p>
+       <p class="journal__entry> Entry: ${entriesObj.entry} </p>
+        <p class="journal__mood> Mood: ${entriesObj.mood} </p>
         </div>
           
     `
--- scripts/JournalForm.js
+++ scripts/JournalForm.js
@@ -1,7 +1,12 @@
 import {getEntries,useEntries} from "./JournalDataProvider.js"
-// import {saveJournalEntry} from "./JournalDataProvider.js"
+import {saveJournalEntry} from "./JournalDataProvider.js"
+
 const eventHub = document.querySelector(".main")
 const contentTarget = document.querySelector(".article")
+
+// create note form HTML with inputs and render form to DOM
+// add a click event for when user clicks the submit button
+// submit should grab values from form inputs, build new note object, and POST that note to the API
 
 // get the entriesdata from api
 
@@ -13,37 +18,39 @@ export const JournalFormComponent  = () =>{
 }
 // rendering and put it into DOM
 
-const render = () =>{
+const render = (entries) =>{
+    
     contentTarget.innerHTML = `
     <label for="date1"> Date of Entry :</label>
-    <input type="date" name="date1" id="date1" size="60">
+    <input type="date" name="date1" id="entries--date1" size="60">
 
     <label for="date1"> Concepts Covered :</label>
-    <input type="text" name="Concepts Covered" id="conceptsCovered" size="65" maxlength="70">
+    <input type="text" name="Concepts Covered" id="entries--conceptsCovered" size="65" maxlength="70">
 
     <label for="date1"> Journal Entry: </label> 
-    <input type="text" id="journalEntrydata">
+    <input type="text" id="entries--journalEntrydata">
     <textarea rows="3" cols="3"> </textarea>
 
     <label for="date1"> Mood for the day:</label> 
-    <select id= "Moodfortheday" name="Mood for the day" > 
+    <select id= "entries--Moodfortheday" name="Mood for the day" > 
         <option value="0">Select Mood for the day</option>
         <option value="Happy"> Happy</option>
         <option value="Sad"> Sad</option>
         <option value="Ok"> Ok</option>
         </select>
-        <button id="journalentry">Record Journal Entry</button>`
+        <button id="entries--journalentry">Record Journal Entry</button>`
        }
        
  
 
 eventHub.addEventListener("click",clickEvent =>{
-    if(clickEvent.target.id === "journalentry"){
+    if(clickEvent.target.id === "entries--journalentry"){
+       
     //    grab the input values
-    const date = document.querySelector("#date1").value
-    const concept = document.querySelector("#conceptsCovered").value
-    const entry = document.querySelector("#journalEntrydata").value
-    const mood = document.querySelector("#Moodfortheday").value
+    const date = document.querySelector("#entries--date1").value
+    const concept = document.querySelector("#entries--conceptsCovered").value
+    const entry = document.querySelector("#entries--journalEntrydata").value
+    const mood = document.querySelector("#entries--Moodfortheday").value
     const timestamp = Date.now()
 
     // make a note object
@@ -55,7 +62,8 @@ eventHub.addEventListener("click",clickEvent =>{
         timestamp
     }
     //  console.log(newNote)
-    savenote(newNote)
+    // POST object to database / API / json file
+    saveJournalEntry(newNote)
 }
 })
 
